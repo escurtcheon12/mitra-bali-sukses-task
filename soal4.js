5fc7d42cee8db420c60aed53ea1080777613404f
Rizky Syahroni <52042087+escurtcheon12@users.noreply.github.com>
2026-06-24 10:35:38 +0700

    Implement functions to check anagram matches

---
+++ soal4.js
@@ -0,0 +1,27 @@
+function sortManually(w) {
+    let result = [];
+    
+    for (const i of w) {
+        let idx = result.findIndex(char => char > i);
+        
+        if (idx === -1) {
+            result.push(i);
+        } else {
+            result.splice(idx, 0, i);
+        }
+    }
+    
+    return result.join('');
+}
+
+function anagramMatches(w1, w2) {
+    const start = sortManually(w1);
+    const end = sortManually(w2);
+    
+    // const start = w1.split('').sort().join('');
+    // const end = w2.split('').sort().join('');
+    
+    return start === end;
+}
+
+console.log(anagramMatches('hello', 'world'));
