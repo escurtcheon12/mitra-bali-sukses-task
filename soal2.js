229c1765f487ec1f7c004e3e224a74ef5eeffbcc
Rizky Syahroni <52042087+escurtcheon12@users.noreply.github.com>
2026-06-24 10:34:42 +0700

    Implement fizzBuzz function to generate FizzBuzz output

---
+++ soal2.js
@@ -0,0 +1,12 @@
+function fizzBuzz(n) {
+    let result = [];
+    
+    for (let i = 1; i <= n; i++) {
+        if (i % 3 === 0 && i % 5 === 0) result.push('Fizzbuzz');
+        else if (i % 3 === 0) result.push('Fizz')
+        else if (i % 5 === 0) result.push('Buzz')
+        else result.push(i)
+    }
+    
+    return result.join(', ');
+}
