5caff8ea16915fc01a6c57f494112ffddcdde6fa
Rizky Syahroni <52042087+escurtcheon12@users.noreply.github.com>
2026-06-24 10:40:55 +0700

    Add comments for anagram matching methods
    Added comments to illustrate two methods for sorting strings.

--- soal4.js
+++ soal4.js
@@ -15,9 +15,11 @@ function sortManually(w) {
 }
 
 function anagramMatches(w1, w2) {
+    /* Cara 1 */
     const start = sortManually(w1);
     const end = sortManually(w2);
-    
+
+    /* Cara 2 */
     // const start = w1.split('').sort().join('');
     // const end = w2.split('').sort().join('');
     
